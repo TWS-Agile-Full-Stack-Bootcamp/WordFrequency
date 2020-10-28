91b70edcadbbafb7e0dfdb230314c6f24332d710
Zhen She <neal.seah@gmail.com>
2020-10-28 22:26:21 +0800

    chore: auto commit after renaming

--- rename.sh
+++ rename.sh
@@ -1,5 +1,6 @@
 #! zsh
 
+sed -i "" -e "s/dotnet-project-template-test/$1Test/" -e "s/dotnet-project-template/$1/" .gitignore
 sed -i "" "s/dotnet_project_template_test/$1Test/" dotnet-project-template-test/dotnet-project-template-test.csproj
 sed -i "" "s/dotnet_project_template/$1/" dotnet-project-template/dotnet-project-template.csproj
 sed -i "" "s/Dotnet_project_template_test/$1Test/" dotnet-project-template-test/UnitTest1.cs
@@ -14,3 +15,8 @@ mv dotnet-project-template-test $1Test
 mv dotnet-project-template $1
 mv dotnet-project-template.sln $1.sln
 mv .vs/dotnet-project-template .vs/$1
+
+rm README.md
+rm rename.sh
+git add .
+git commit -m "chore: rename project to $1"
