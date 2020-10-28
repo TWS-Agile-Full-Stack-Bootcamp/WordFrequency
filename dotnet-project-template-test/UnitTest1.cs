1db68b18a78bfc4f807157b32ae78f5a7c234373
xuchen <xuchen@thoughtworks.com>
2020-10-28 14:16:39 +0800

    [chennxu]: add stylecop and rule set to test project

--- dotnet-project-template-test/UnitTest1.cs
+++ dotnet-project-template-test/UnitTest1.cs
@@ -1,15 +1,14 @@
-using Dotnet_project_template;
-using System;
-using Xunit;
-
-namespace dotnet_project_template_test
+namespace Dotnet_project_template_test
 {
+    using Dotnet_project_template;
+    using Xunit;
+
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-            var class1= new Class1();
+            var class1 = new Class1();
             Assert.NotNull(class1);
         }
     }
