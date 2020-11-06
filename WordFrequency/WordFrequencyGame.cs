021e579a5eac7d5b4c5ffdb545df42fc25f8d2b9
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-06 15:07:19 +0800

    refactor: remove unnecessary try catch

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -8,47 +8,40 @@ namespace WordFrequency
     {
         public string GetResult(string inputStr)
         {
-                try
-                {
-                    //split the input string with 1 to n pieces of spaces
-                    string[] words = Regex.Split(inputStr, @"\s+");
+            //split the input string with 1 to n pieces of spaces
+            string[] words = Regex.Split(inputStr, @"\s+");
 
-                    List<WordFrequency> wordFrequencies = new List<WordFrequency>();
-                    foreach (var word in words)
-                    {
-                        WordFrequency wordFrequency = new WordFrequency(word, 1);
-                        wordFrequencies.Add(wordFrequency);
-                    }
+            List<WordFrequency> wordFrequencies = new List<WordFrequency>();
+            foreach (var word in words)
+            {
+                WordFrequency wordFrequency = new WordFrequency(word, 1);
+                wordFrequencies.Add(wordFrequency);
+            }
 
-                    //get the map for the next step of sizing the same word
-                    Dictionary<string, List<WordFrequency>> groupedWordFrequencies = GroupByWord(wordFrequencies);
+            //get the map for the next step of sizing the same word
+            Dictionary<string, List<WordFrequency>> groupedWordFrequencies = GroupByWord(wordFrequencies);
 
-                    List<WordFrequency> list = new List<WordFrequency>();
-                    foreach (var entry in groupedWordFrequencies)
-                    {
-                        WordFrequency wordFrequency = new WordFrequency(entry.Key, entry.Value.Count);
-                        list.Add(wordFrequency);
-                    }
+            List<WordFrequency> list = new List<WordFrequency>();
+            foreach (var entry in groupedWordFrequencies)
+            {
+                WordFrequency wordFrequency = new WordFrequency(entry.Key, entry.Value.Count);
+                list.Add(wordFrequency);
+            }
 
-                    wordFrequencies = list;
+            wordFrequencies = list;
 
-                    wordFrequencies.Sort((w1, w2) => w2.WordCount - w1.WordCount);
+            wordFrequencies.Sort((w1, w2) => w2.WordCount - w1.WordCount);
 
-                    List<string> strList = new List<string>();
+            List<string> strList = new List<string>();
 
-                    //stringJoiner joiner = new stringJoiner("\n");
-                    foreach (WordFrequency wordFrequency in wordFrequencies)
-                    {
-                        string s = wordFrequency.Word + " " + wordFrequency.WordCount;
-                        strList.Add(s);
-                    }
+            //stringJoiner joiner = new stringJoiner("\n");
+            foreach (WordFrequency wordFrequency in wordFrequencies)
+            {
+                string s = wordFrequency.Word + " " + wordFrequency.WordCount;
+                strList.Add(s);
+            }
 
-                    return string.Join("\n", strList.ToArray());
-                }
-                catch (Exception e)
-                {
-                    return "Calculate Error";
-                }
+            return string.Join("\n", strList.ToArray());
         }
 
         private Dictionary<string, List<WordFrequency>> GroupByWord(List<WordFrequency> wordFrequencies)
