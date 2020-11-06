e938fd211263a83e6d6b6011197d794b97ade6ff
Chen Xu <xuchen@thoughtworks.com>
2020-11-06 15:08:37 +0800

    refactor: extra split by space method

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -10,53 +10,51 @@ namespace WordFrequency
 
         public string GetResult(string inputStr)
         {
-            if (Regex.Split(inputStr, SpacePatten).Length == 1)
+            //split the input string with 1 to n pieces of spaces
+            string[] splitWords = Regex.Split(inputStr, SpacePatten);
+
+            if (splitWords.Length == 1)
             {
                 return inputStr + " 1";
             }
-            else
+
+            try
             {
-                try
+                List<Input> inputList = new List<Input>();
+                foreach (var s in splitWords)
                 {
-                    //split the input string with 1 to n pieces of spaces
-                    string[] arr = Regex.Split(inputStr, @"\s+");
-
-                    List<Input> inputList = new List<Input>();
-                    foreach (var s in arr)
-                    {
-                        Input input = new Input(s, 1);
-                        inputList.Add(input);
-                    }
-
-                    //get the map for the next step of sizing the same word
-                    Dictionary<string, List<Input>> map = GetListMap(inputList);
-
-                    List<Input> list = new List<Input>();
-                    foreach (var entry in map)
-                    {
-                        Input input = new Input(entry.Key, entry.Value.Count);
-                        list.Add(input);
-                    }
-
-                    inputList = list;
-
-                    inputList.Sort((w1, w2) => w2.WordCount - w1.WordCount);
-
-                    List<string> strList = new List<string>();
-
-                    //stringJoiner joiner = new stringJoiner("\n");
-                    foreach (Input w in inputList)
-                    {
-                        string s = w.Value + " " + w.WordCount;
-                        strList.Add(s);
-                    }
-
-                    return string.Join("\n", strList.ToArray());
+                    Input input = new Input(s, 1);
+                    inputList.Add(input);
                 }
-                catch (Exception e)
+
+                //get the map for the next step of sizing the same word
+                Dictionary<string, List<Input>> map = GetListMap(inputList);
+
+                List<Input> list = new List<Input>();
+                foreach (var entry in map)
                 {
-                    return "Calculate Error";
+                    Input input = new Input(entry.Key, entry.Value.Count);
+                    list.Add(input);
                 }
+
+                inputList = list;
+
+                inputList.Sort((w1, w2) => w2.WordCount - w1.WordCount);
+
+                List<string> strList = new List<string>();
+
+                //stringJoiner joiner = new stringJoiner("\n");
+                foreach (Input w in inputList)
+                {
+                    string s = w.Value + " " + w.WordCount;
+                    strList.Add(s);
+                }
+
+                return string.Join("\n", strList.ToArray());
+            }
+            catch (Exception e)
+            {
+                return "Calculate Error";
             }
         }
 
